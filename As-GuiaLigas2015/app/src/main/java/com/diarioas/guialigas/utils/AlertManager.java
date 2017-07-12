0185c8689ec7ae48beead915dc0cfa9e937ce14c
dani.jimenez <daniel.jimenez@sdos.es>
2017-07-12 10:00:39 +0200

    MOVAS03-5 -Agregada una opción de diálogo para manejar los errores de certificado ssl

--- As-GuiaLigas2015/app/src/main/java/com/diarioas/guialigas/utils/AlertManager.java
+++ As-GuiaLigas2015/app/src/main/java/com/diarioas/guialigas/utils/AlertManager.java
@@ -16,16 +16,21 @@ import com.diarioas.guialigas.R;
  */
 public class AlertManager {
 
-	public static void showAlertOkDialog(Context ctx, String message,
-			String title, DialogInterface.OnClickListener listener) {
+	public static void showCancleableDialog(Context ctx, String message, String title,
+											DialogInterface.OnClickListener listener,
+											boolean cancelable) {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctx);
 		alertDialogBuilder.setTitle(title);
 
 		alertDialogBuilder.setMessage(message);
 
-		alertDialogBuilder.setIcon(R.drawable.icon).setCancelable(false)
-				.setNeutralButton(R.string.accept, listener);
+		alertDialogBuilder.setIcon(R.drawable.icon).setCancelable(cancelable)
+				.setPositiveButton(R.string.accept, listener);
+
+		if (cancelable) {
+			alertDialogBuilder.setNegativeButton(R.string.cancel, listener);
+		}
 
 		AlertDialog alertDialog = alertDialogBuilder.create();
 
@@ -34,5 +39,11 @@ public class AlertManager {
 		}
 
 		return;
+	}
+
+	public static void showAlertOkDialog(Context ctx, String message,
+										 String title, DialogInterface.OnClickListener listener) {
+
+		showCancleableDialog(ctx, message, title, listener, false);
 	}
 }
